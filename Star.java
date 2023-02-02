26c7198dcf3d03740646ddededda1715727a9391
mukeshkr8434 <mukeshkr8434@gmail.com>
2023-02-02 14:35:45 +0530

    first commit

---
+++ Star.java
@@ -0,0 +1,19 @@
+Class Stardemo1
+{
+public static void main (String args[] )
+ {
+	 int a = 5;
+	 for ( int i = 1; i<=a; i++)
+	 {
+		for( int j = a-1; j>=1; j++) 
+		{
+			System.out.print("");
+		}
+		for(int k = 1; k <= 1; k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	 }
+ }
+}
